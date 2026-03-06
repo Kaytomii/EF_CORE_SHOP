1c8e04f42450bedfd4129eaef460cb67ff3c5ba1
Yan Tarasov <vzu3427@gmail.com>
2026-03-06 16:56:43 +0200

    Added Data annotation for field

--- Shop.Domain/Entities/Product.cs
+++ Shop.Domain/Entities/Product.cs
@@ -1,9 +1,18 @@
-﻿namespace Shop.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Domain.Entities;
 
 public class Product
 {
+    [Key]
     public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(1000, ErrorMessage = "Name cannot exceed 1000 characters")]
     public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [Range(0, 10000000, ErrorMessage = "Price must be greater than 0")]
     public decimal Price { get; set; }
     public DateTime CreatedAt { get; set; }
 
